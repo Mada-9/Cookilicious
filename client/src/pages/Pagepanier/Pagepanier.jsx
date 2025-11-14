7f6f7f16197ee2d0221bb7719b03f4aabbe5298d
Madama <madaac92@gmail.com>
2025-11-14 21:42:00 +0100

    commencement de la page panier

--- client/src/pages/Pagepanier/Pagepanier.jsx
+++ client/src/pages/Pagepanier/Pagepanier.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useCart } from "react-use-cart";
 
 import "./panier.css"; // CSS spécifique
 
 const Pagepanier = () => {
+  const {
+    
+    isEmpty,
+    totalItems,
+    totalUniqueItems,
+    items,
+    inCart,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+    
+  } = useCart();
 
+  return(
+   <div>
 
+    <h1>Panier</h1>
+    {!isEmpty ?
+     <p>le panier est vide</p>: 
+    
+    <p>Total des articles {totalUniqueItems}, total {totalItems}</p>
+    }
+    </div>)
+};
 
-  return (
-    <div>   Pagepanier
-        <h1>Panier</h1>
-    </div>
-  )
-}
-
-export default Pagepanier
+export default Pagepanier;
