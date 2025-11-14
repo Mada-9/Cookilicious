c08f6883d7fb1b9ec25878a1b3c8a8e23d0f923a
Madama <madaac92@gmail.com>
2025-11-14 21:49:50 +0100

    Sauvegarde avant de passer sur la branche produit

--- client/src/pages/PageProduit/DetailProduit.jsx
+++ client/src/pages/PageProduit/DetailProduit.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import URL from "../../utils/constant/url";
 import Produit from "./Produit";
+import { CartProvider, useCart } from "react-use-cart";
 
 const DetailProduit = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const params = useParams();
   const { id } = params;
   const [produit, setProduit] = useState(null);
+  const {  addToCart } = useCart();
 
   useEffect(() => {
     if (id) {
@@ -42,8 +44,8 @@ const DetailProduit = () => {
               style={{
                 color: "var(--creme)",
                 // backgroundColor: " #67200dff",
-                backgroundColor: "var(--marronFroid)",
-                height:"6rem",
+                backgroundColor: "var(--marronRouge)",
+                height: "6rem",
                 marginTop: "3rem",
                 fontSize: "3.5rem",
                 width: "90%",
@@ -59,7 +61,7 @@ const DetailProduit = () => {
               }}
             >
               <img
-                style={{ border: "2px #976658ff solid " }}
+                style={{ border: "5px var(--marronRouge) solid " }}
                 src={produit.photo}
                 alt={produit.titre}
                 width={460}
@@ -67,7 +69,7 @@ const DetailProduit = () => {
               />
               <div style={{ padding: "3rem" }}>
                 <p style={{ fontSize: "3.5rem" }}>{produit.prix} €</p>
-                <button style={{ height:"3rem", width:"4rem"}}></button>
+                <button style={{ height: "3rem", width: "4rem" }}></button>
                 <p>{produit.description}</p>
                 <button
                   style={{
@@ -78,10 +80,10 @@ const DetailProduit = () => {
                     // backgroundColor: " #976658ff",
                     border: "var(--marronRouge) 3px solid",
                     backgroundColor: "var(--jaune)",
-                  }}
-                  onClick={() => navigate("/panier")}
+                   type:"submit"}}
+                  onClick={()=>addToCart(produit.id)}
                 >
-                  Ajouter au panier
+                  Add to cart Ajouter au panier
                 </button>
               </div>
             </div>
