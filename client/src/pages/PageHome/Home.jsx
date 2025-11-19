b4d9fe0b64a6bdbb5a46017e87cd5f7fb9d8649b
Madama <madaac92@gmail.com>
2025-11-19 18:49:37 +0100

    Debut modification bootstrap de la page home

--- client/src/pages/PageHome/Home.jsx
+++ client/src/pages/PageHome/Home.jsx
@@ -12,6 +12,7 @@ import URL from "../../utils/constant/url";
 import cookie from "../../assets/images/cookiepistache.png";
 import cookiefraise from "../../assets/images/cookiefraise.png";
 import cookiechoco from "../../assets/images/cookiechoco.png";
+import cookie4 from "../../assets/images/CaptureE299e81cran20a80_11.53.01-removebg-preview.png";
 
 //CSS
 import "./Home.css"; // CSS spécifique
@@ -20,6 +21,7 @@ const PageHome = ({}) => {
   const navigate = useNavigate();
   const [produit, setProduit] = useState([]);
   const [formData, setFormData] = useState({ email: "", message: "" });
+  const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
   // MEDIA QUERY
 
@@ -34,7 +36,7 @@ const PageHome = ({}) => {
 
   // POUR DETECTER LA TAILLE DE L'ECRAN
 
-  //MON CSS
+  //a refaire dans  CSS, (bootstrap)row
 
   const styles = {
     titleHome: {
@@ -52,12 +54,9 @@ const PageHome = ({}) => {
       color: "var(--marronRouge)",
     },
 
-    
     // phraseDefilante: {
     //   fontSize: isMobile ? "1rem" : "1.7rem",
     // },
-
-  
   };
 
   useEffect(() => {
@@ -77,20 +76,30 @@ const PageHome = ({}) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormData((prevformData) => ({ ...prevformData, [name]: value }));
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      const { data, status } = await axios.post(URL.POST_CONTACT, formData);
+      const { status } = await axios.post(URL.POST_CONTACT, formData);
       console.log(formData);
-      if (status === 200) {
-        console.log("Données du formualire bien récupérées:");
+      if (
+        formData.message.length >= 5 &&
+        status === 201 &&
+        formData.email.match(isValidEmail)
+      ) {
+        alert("message envoyé !");
+      } else {
+        alert("veuillez ecrire un message valide");
       }
     } catch (error) {
       console.error("erreur lors de l'envoie du message:", error.message);
     }
   };
+
+  // BACK TO TOP
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
@@ -101,8 +110,8 @@ const PageHome = ({}) => {
         <h1 style={styles.titleHome}>COOKILICIOUS</h1>
 
         <div className="imageHome" onClick={() => navigate("/produit")}>
-          <img className="imagesHome" src={cookiefraise} alt="COOKIE" />
-          <img className="imagesHome" src={cookie} alt="COOKIE" />
+          <img className="imagesHome" src={cookiechoco} alt="COOKIE" />
+          <img className="imagesHome" src={cookiechoco} alt="COOKIE" />
           <img className="imagesHome" src={cookiechoco} alt="COOKIE" />
         </div>
 
@@ -113,24 +122,28 @@ const PageHome = ({}) => {
           </p>
         </div>
 
-        <div className="homeCategorieProduit">
-          <h2
-            className="phraseCategorie"
-          >
+        <div className="homeCategorieProduit row ">
+          <h2 className="phraseCategorie col-sm-10 col-md-4">
             Parcourez nos différentes catégories, <br /> et <br /> laissez vous
             succomber...
           </h2>
 
-          <section className="sectionCategorie">
+          <section
+            className="sectionCategorie col-sm-6 col-md-3 "
+            id="categorieUne"
+          >
             COOKIE
-            {/*  HASHLINK  NE FONCTIONNE PAS*/}
-            <HashLink smooth to="/produit#">
+            <HashLink smooth to="/produit#cookie">
               <button className="btnCommander">Commander</button>
             </HashLink>
           </section>
-          <section className="sectionCategorie">
+          <section
+            className="sectionCategorie col-sm-6 col-md-3"
+            id="categorieDeux"
+          >
             BROOKIE
-            <HashLink smooth to="/produit#brookie">
+            <HashLink smooth to="/produit#sectionBrookie">
+              {/*  HASHLINK  NE FONCTIONNE PAS*/}
               <button className="btnCommander">Commander</button>
             </HashLink>
           </section>
@@ -139,12 +152,9 @@ const PageHome = ({}) => {
         <div className="homeRecipes">
           <h2 className="homeRecipesTitle">Testez nos recettes</h2>
 
-          <div  className="recetteHome">
+          <div className="recetteHome">
             (mettre slide recettes )
-            <button
-              onClick={() => navigate("/recette")}
-              className="btnRecette"
-            >
+            <button onClick={() => navigate("/recette")} className="btnRecette">
               Je découvre !
             </button>
           </div>
@@ -158,7 +168,7 @@ const PageHome = ({}) => {
           />
         </div>
 
-        <div  className="containerJaune">
+        <div className="containerJaune">
           <h3 className="jauneP">
             Lorem ipsum dolor sit amet consectetur adipisicing Corporis.
             <br /> elit Corporis itaqueimpedit <br /> bralias mollitia,
@@ -168,60 +178,59 @@ const PageHome = ({}) => {
 
         {/* PRODUITS AFFICHES SUR HOME */}
 
-        <div className="homeProduit">
-          <h2 className="homeProduitTitle">One more sweetness...</h2>
-          <div className="containerProduit">
+        <div className="homeProduit row">
+          <h2 className="homeProduitTitle col-sm-12 ">One more sweetness...</h2>
+          <div className="containerProduit ">
             <div
+              className="carousel slide "
               id="carouselProduit"
-              className="carousel slide"
               data-bs-ride="carousel"
             >
-              <div className="carousel-inner" style={{ marginBottom: "2rem" }}>
-                {produit.length > 0 &&
-                  [...Array(Math.ceil(produit.length / 4))].map(
-                    (_, slideIndex) => (
-                      <div
-                        key={slideIndex}
-                        className={`carousel-item ${
-                          slideIndex === 0 ? "active" : ""
-                        }`}
-                      >
-                        <div
-                          className="d-flex "
-                          style={{ gap: "2rem", padding: "2rem" }}
-                        >
-                          {produit
-                            .slice(slideIndex * 4, slideIndex * 4 + 4)
-                            .map((item) => (
-                              <div key={item._id} className="produitCard">
-                                <p>{item.titre}</p>
-                                <img
-                                  src={item.photo}
-                                  alt={item.titre}
-                                  width={150}
-                                  height={150}
-                                />
-                                <p>{item.prix} €</p>
-                                <button
-                                 className="produitCardBtn"
-                                  onClick={() =>
-                                    navigate(`/detail/${item._id}`)
-                                  }
-                                >
-                                  Voir
-                                </button>
-                              </div>
-                            ))}
-                        </div>
+              <div className="carousel-inner mx-auto row ">
+                {produit
+                  .filter((_, i) => i % 4 === 0) // on prend un produit sur 4 = début d'une diapo
+                  .map((_, i) => (
+                    <div
+                      key={i}
+                      className={`carousel-item ${i === 0 ? "active" : ""}`}
+                    >
+                      <div className="d-flex" style={{}}>
+                        {produit
+                          .slice(i, i + 4) // on prend 4 produits
+                          .map((item) => (
+                            <div
+                              key={item._id}
+                              className="produitCard col-sm-11 col-md-3"
+                            >
+                              <p>{item.titre}</p>
+                              <img
+                                className="img-fluid"
+                                src={
+                                  item.photo ||
+                                  "https://static.wixstatic.com/media/82955a_99098664b7034f9b876c2b43ac70d615~mv2.jpg/v1/crop/x_71,y_71,w_938,h_938/fill/w_938,h_938,al_c,q_85,enc_avif,quality_auto/Cooies_Puffy2.jpg"
+                                }
+                                alt={item.titre}
+                                width={150}
+                                height={150}
+                              />
+                              <p>{item.prix} €</p>
+                              <button
+                                className="produitCardBtn"
+                                onClick={() => navigate(`/detail/${item._id}`)}
+                              >
+                                Voir
+                              </button>
+                            </div>
+                          ))}
                       </div>
-                    )
-                  )}
+                    </div>
+                  ))}
               </div>
             </div>
- {/* BOUTON CARROUSEL */}
+            {/* BOUTON CARROUSEL */}
             <div>
               <button
-                className="carousel-control-prev  "
+                className="carousel-control-prev "
                 style={{
                   height: "2rem",
                   marginTop: "35rem",
@@ -238,14 +247,13 @@ const PageHome = ({}) => {
                 ></span>
                 <span className="visually-hidden">Previous</span>
               </button>
-
               <button
                 className="carousel-control-next"
                 style={{
                   height: "2rem",
                   marginTop: "35rem",
                   marginRight: "20rem",
-                  color: "var(--marronFonce)"
+                  color: "var(--marronFonce)",
                 }}
                 type="button"
                 data-bs-target="#carouselProduit"
@@ -258,18 +266,12 @@ const PageHome = ({}) => {
                 <span className="visually-hidden">Next</span>
               </button>
             </div>
-
-            <img
-              className="moitieeCookieProduit"
-              src={cookie}
-              alt="cookies pistache"
-            />
           </div>
         </div>
 
         {/* SECTION CONTACT */}
 
-        <div className="contactHome" style={{ marginBottom: "5rem" }}>
+        <div className="contactHome">
           <p className="homeContactTitle">Contactez nous</p>
           <form className="homeFormContact" onSubmit={handleSubmit}>
             <label className="email" htmlFor="email">
@@ -278,11 +280,10 @@ const PageHome = ({}) => {
             <input
               type="email"
               name="email"
-              id="email"
+              className="emailInput"
               placeholder="écrivez votre email"
               value={formData.email}
               onChange={handleChange}
-              style={{ color: "var(--marronFroid)", fontSize: "1.5rem" }}
             />
             <label className="message" htmlFor="message">
               Message:
@@ -290,17 +291,12 @@ const PageHome = ({}) => {
             <input
               type="texte"
               name="message"
-              id="message"
+              className="messageInput"
               placeholder="Votre message"
               value={formData.message}
               onChange={handleChange}
-              style={{ color: "var(--marronFroid)", fontSize: "1.5rem" }}
             />
-            <button
-              id="homeBtnContact"
-              onSubmit={handleSubmit}
-              style={{ color: "var(--marronFroid)", fontSize: "1.6rem" }}
-            >
+            <button className="homeBtnContact" onSubmit={handleSubmit}>
               Envoyer
             </button>
           </form>
@@ -318,8 +314,7 @@ const PageHome = ({}) => {
           <span> . </span>COOKILIOUS<span> . </span>COOKILIOUS<span> . </span>
           COOKILIOUS<span> . </span>COOKILIOUS<span> . </span>COOKILIOUS
           <span> . </span>COOKILIOUS<span> . </span>COOKILIOUS<span> . </span>
-          COOKILIOUS<span> . </span>COOKILIOUS<span> . </span>COOKILIOUS
-          <span> . </span>COOKILIOUS<span> . </span>COOKILIOUS
+          COOKILIOUS<span> . </span>COOKILIOUS
         </p>
       </div>
     </>
