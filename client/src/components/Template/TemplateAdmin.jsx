4968bb3f9daa62ca51f20091000fa6db0db61bdf
Madama <madaac92@gmail.com>
2025-12-04 14:57:44 +0100

    updated files

--- client/src/components/Template/TemplateAdmin.jsx
+++ client/src/components/Template/TemplateAdmin.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import {Outlet} from "react-router-dom";
 import Header from "../Admin/HeaderAdmin";
@@ -22,23 +21,3 @@ const TemplateAdmin = () => {
 
 
 export default TemplateAdmin;
-=======
-import React from 'react'
-import NavAdmin from '../Admin/NavAdmin'
-import { Outlet } from 'react-router-dom'
-
-
-const TemplateAdmin = () => {
-  return (
-    <div>
-        <NavAdmin/>
-        <section>
-            <Outlet/>
-        </section>
-    
-    </div>
-  )
-}
-
-export default TemplateAdmin
->>>>>>> 1b20691f17c33f2ba5dd0f904bf7942c6deb498e
