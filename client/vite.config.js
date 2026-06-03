8f3aa29aedb32d7ce0c60dcd24ad707d763597f8
Madama <madaac92@gmail.com>
2026-06-03 20:53:07 +0200

    new update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Version recommandée pour Vite
 const API_URL = import.meta.env.MODE === "development"
   ? "http://localhost:8000"
-  : "cookiliciousbackend.vercel.app" // <-- Remplace par ton vrai lien backend
+  : "https://cookilicious-backend.vercel.app"  // <-- Remplace par ton vrai lien backend
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
